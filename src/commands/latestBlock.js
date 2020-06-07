ceee5b11705ed8023a6c7abca1f37ea2338921d5
Anton Buenavista <ay.buenavista@gmail.com>
2020-06-07 23:41:31 +0800

    Added /latestBlock command

--- src/commands/latestBlock.js
+++ src/commands/latestBlock.js
@@ -4,11 +4,21 @@ module.exports = () => {
   return async ctx => {
     const { helpers, message, replyWithMarkdown, state } = ctx;
     const { inReplyTo } = Extra;
+    const { getWeb3 } = helpers;
     const { args } = state.command;
 
     const network = args[0] ? args[0].toLowerCase() : 'polkadot';
-    const web3 = helpers.getWeb3(network);
+    const web3 = getWeb3(network);
+    const lastHeader = await web3.rpc.chain.getHeader();
+    const header = await web3.derive.chain.getHeader(lastHeader.hash);
 
-    replyWithMarkdown(`*${web3.consts.system.blockHashCount.toNumber()}*`, inReplyTo(message.message_id));
+    let msg = '';
+    msg = msg.concat(
+      `Latest Block #: \`${lastHeader.number}\`\n`,
+      `Hash: \`${lastHeader.hash}\`\n`,
+      `Author: \`${header.author}\``,
+    );
+
+    replyWithMarkdown(msg, inReplyTo(message.message_id));
   };
 };
