2c65f14f712ed7ca7bbef5bc6ac715d7680ebede
Anton Buenavista <ay.buenavista@gmail.com>
2020-06-09 01:42:31 +0800

    Modified /chainCandidate to /chainInfo

---
+++ src/commands/chainInfo.js
@@ -0,0 +1,40 @@
+const Extra = require('telegraf/extra');
+
+module.exports = () => {
+  return async ctx => {
+    const { helpers, message, replyWithMarkdown, state } = ctx;
+    const { inReplyTo } = Extra;
+    const { getWeb3 } = helpers;
+    const { args } = state.command;
+
+    const network = args[0] ? args[0].toLowerCase() : 'polkadot';
+    const web3 = getWeb3(network);
+
+    const [chain, totalIssuance, session] = await Promise.all([
+      web3.rpc.system.chain(),
+      web3.query.balances.totalIssuance(),
+      web3.derive.session.info()
+    ]);
+    console.log(parseInt(JSON.stringify(session.activeEraStart)));
+
+    let msg = '';
+    msg = msg.concat(
+      `Chain: \`${chain}\`\n`,
+      `Total Issuance: \`${totalIssuance.toHuman()}\`\n`,
+      `Active Era: \`${session.activeEra.toHuman()}\`\n`,
+      `Start of Active Era: \`${
+        new Date(
+          parseInt(JSON.stringify(session.activeEraStart))
+        ).toLocaleDateString()
+      }\`\n`,
+      `Current Era: \`${session.currentEra.toHuman()}\`\n`,
+      `Current Index: \`${session.currentIndex.toHuman()}\`\n`,
+      `Era Length: \`${session.eraLength.toHuman()}\`\n`,
+      `Session Length: \`${session.sessionLength.toHuman()}\`\n`,
+      `Session Per Era: \`${session.sessionsPerEra.toHuman()}\`\n`,
+      `Validator Count: \`${session.validatorCount.toHuman()}\`\n`,
+    );
+
+    replyWithMarkdown(msg, inReplyTo(message.message_id));
+  };
+};
